fe28927b03ad89b50e7500f4e80ef2fe55bd439f
Atqiah-Rahman-Reesha <68156676+Atqiah-Rahman-Reesha@users.noreply.github.com>
2020-08-20 23:55:12 +0600

    1st project

---
+++ about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+ <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">World View</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>FOODIE US</h1>
+  <p>a Reach Food Service was founded by a team of professionals with a passion for world cuisine and the expertise to support the most creative, innovative chefs in the world. 
+
+Created around the Gourmet Seafood division as the core business, REACH caters to the premium end of the London Food service market.  The product range is a well-balanced mix of both imported and locally sourced fish and shellfish.   We have expanded our product range to include fine pastry ingredients and artisanal cheeses
+
+With an eye for quality and a sense of adventure, our passion has taken us beyond the frontiers of England to create a portfolio of seafood from the exotic seafood markets of the Far East, from the extensive shrimp farms in South East Asia, and from the depths of the of the icy cold Antarctic seas.   </p>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+  <div class="col-lg-6 col-md-6 col-12">
+    <img src="image/b2.jpg" class="img-fluid aboutimg">
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <h4>Food will be in your doorstep</h4>
+    <p class="py-5">On the consumer end of the value chain, our team of specialists work collaboratively with our customers by combining category knowledge, supply chain insights and food service trends to create a highly differentiated food service experience. The ‘REACH’ Team is also competent to offer innovative and cost-effective menu solutions, which is reinforced by a fresh take on sustainability. Our customer-focused approach to doing business and an uncompromising attitude to service have distinguished us as the preferred supplier to the Chef community we serve!  .</p>
+  
+  </div>
+</div>
+</div>
+<footer>
+    <p class="p-3 bg-dark text-white text-center">@theFoodieUsProduction</p>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,232 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,700&display=swap" rel="stylesheet">
+  
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">FOODIE US &#129346</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact Us</a>
+      </li>
+    
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+   
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="image/a4.jpg" alt="Welcome to Foodies world" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1>Welcome to Foodies world</h1>
+        <h3>Are you Hungry?</h3>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/a3.jpg" alt="Food Is a Magic!" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1>Food Is a Magic!</h1>
+        <h3>What do you think?</h3>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="image/a5.jpg" alt="Food can make you happy within a second" width="1100" height="500">
+      <div class="carousel-caption">
+        <h1>Food can make you happy within a second</h1>
+        <h3>Are you interested on food?</h3>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+  <div class="row">
+    <div class="log-lg-6 col-md-6 col-12">
+      <img src="image/b2.jpg" class="img-fluid aboutimg" >
+    </div>
+    <div class="log-lg-6 col-md-6 col-12">
+      <h3>Hello Everyone, &#128587;</h3><br>
+     <p class="py-3"> Welcome to our website. We are here to serve you your favourite dishes. <br>
+      <pre>      <b>*Are you hungry?&#129383;
+           *Want to surprise mom?&#129346;
+           *Missing home food?&#128148;
+           *Feeling bored?&#128583;
+           *Stove off?&#128293;</b>
+           </pre></p>
+           <p> Don't worry! We are here. just call us & chill &#129299;<br>
+            we are always here for your service. <br>
+            <b> Then What Are U Waiting For? &#129331;</b></p>
+            <a href="about.php" class="btn-btn-success"> Check More </a>
+    </div>
+  </div>
+</div>
+</section>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Our Services</h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card">
+  <img class="card-img-top" src="image/a7.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Breakfast</h4>
+    <p class="card-text">Start with your day with a healthy meal</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+        
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card">
+  <img class="card-img-top" src="image/a9.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Lunch</h4>
+    <p class="card-text">Refresh Your mind and give energy to your body</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+        
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-12">
+        <div class="card">
+  <img class="card-img-top" src="image/b3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Dinner</h4>
+    <p class="card-text">A light meal can balance your diet along with your cravings</p>
+    <a href="#" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+        
+      </div>
+
+    </div>
+  </div>
+  </section>
+
+  <section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Gallery</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b5.jpg " class="simg1">
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/1.3.jpg "class="simg2">
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/1.5.jpg "class="simg3">
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/1.8.jpg "class="simg4">
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b1.jpg "class="simg5">
+      </div>
+       <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b4.jpg "class="simg6">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b6.jpg "class="simg7">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b7.jpg "class="simg8">
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+        <img src="image/b8.jpg "class="simg9">
+      </div>
+
+    </div>
+    
+  </div>
+</section>
+
+ <section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">Contact Us</h2>
+    <h3><b> Email:</b><a href="a.rahman.reesha@gmail.com">a.rahman.reesha@gmail.com</a></h3>
+  <h3>Mobile: 017xxxxxxxx</h3>
+  <h3>Want to order? &#129331;</h3>
+  </div>
+  <div class="w-50 m-auto">
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+      <lebel>Username</lebel>
+      <input type="text" name="user" autocomplete="off" class="form-control">
+    </div>
+    <div class="form-group">
+      <lebel>Email Id</lebel>
+      <input type="text" name="email" autocomplete="off" class="form-control">
+    </div>
+    <div class="form-group">
+      <lebel>Mobile</lebel>
+      <input type="text" name="mobile" autocomplete="off" class="form-control">
+    </div>
+     <div class="form-group">
+      <lebel>Place Order</lebel>
+      <textarea class="form-control" name="comment"></textarea>
+    </div>
+    <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+  </div>
+</section>
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@theFoodieUsProduction</p>
+</footer>
+  
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  
+  </body>
+  </html>
